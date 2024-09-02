67e98a89e614b8a0ebffd87956c197c605edad0f
Tobias-Henriksen <at101305@edu.aarhustech.dk>
2024-09-02 15:09:29 +0200

    now using plotly

--- weather2.py
+++ weather2.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from meteostat import Point, Daily
 
 # Set time period
+#
+
 yearStart = int(input("Start årstal: "))
 monthStart=int(input("Start måned: "))
 dayStart=int(input("Start dag: "))
@@ -11,6 +13,15 @@ dayStart=int(input("Start dag: "))
 yearEnd = int(input("Slut årstal: "))
 monthEnd=int(input("Slut måned: "))
 dayEnd=int(input("Slut dag: "))
+'''
+yearStart = 2024
+monthStart=1
+dayStart=1
+
+yearEnd = 2024
+monthEnd=2
+dayEnd=1
+'''
 # Set time period
 start = datetime(yearStart, monthStart, dayStart)
 end = datetime(yearEnd, monthEnd, dayEnd)
@@ -21,7 +32,10 @@ location = Point(49.2497, -123.1193, 70)
 # Get daily data for 2018
 data = Daily(location, start, end)
 data = data.fetch()
+print(data)
 
 # Plot line chart including average, minimum and maximum temperature
-data.plot(y=['tavg', 'tmin', 'tmax'])
-plt.show()
+#data.plot(y=['tavg', 'tmin', 'tmax'])
+#plt.show()
+fig = px.line(data,y=['tavg','prcp'],title="nej")
+fig.show()
